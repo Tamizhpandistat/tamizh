3276dd926e81df79d6923adeb757533adbce9312
asus <tamilpandi1411@gmail.com>
2025-05-23 19:14:38 +0530

    add file

---
+++ objects.py
@@ -0,0 +1,10 @@
+from bankaccount import *
+
+tamizh = BankAccount("tamizh",1000)
+pandi = BatnkAccount("pandi",2000)
+tamizh.getBalance()
+tamizh.deposit(2000)
+tamizh.withdraw(3001)
+pandi.getBalance()
+pandi.deposit(1000)
+pandi.withdraw(2000)
